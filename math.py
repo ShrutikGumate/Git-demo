f7c6f272d2720d31b8eb6ff2e28a89b9f4bed7d2
Shrutik Gumate <53931143+ShrutikGumate@users.noreply.github.com>
2021-05-28 14:38:19 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,19 @@
+# Add implemention
+def add(x,y):
+	return x + y
+# Subtract implemention
+def sub(x,y):
+	return x - y       # on master branch
+# Multiply implemention
+def mul(x,y):
+	return x*y      
+# Division implemention
+def div(x,y):
+	if y==0:
+        return "Error_Divide_0"
+    else:
+        return x/y
+
+# Square implemention
+def square(x):
+    pass
